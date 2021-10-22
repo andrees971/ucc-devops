8ba2061b27f580acc355876d1abffe9a3e7e307a
andrees971 <andres.silvamunoz@campusucc.edu.co>
2021-10-21 20:51:19 -0500

    se agrgego archivo dividido de area.cpp

--- area.cpp
+++ area.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 
-    float areaCuadrado, areaCirculo, areaTriangulo, l1, radio, h, base;
+    float areaCuadrado, areaCirculo, areaTriangulo, l1, radio, h, base, sqrAreaCuadrado, sqrAreaTriangulo, sqrAreaCirculo;
 
     l1 = 5;
     radio = 8;
@@ -14,13 +14,26 @@ int main()
     
 
     areaCuadrado = l1 * l1;
-    printf("La raiz del area del cuadrado es: %f", sqrt(areaCuadrado));
+    printf("el area del cuadrado es: %f", areaCuadrado);
 
     /*Triangulo*/
     areaTriangulo = (base * h) / 2;
-    printf("\nLa raiz del del area del triangulo es de: %f", sqrt(areaTriangulo));
+    printf("\nel area del triangulo es de: %f", areaTriangulo);
 
     /*Circulo*/
     areaCirculo = (radio * radio) * 3.14;
-    printf("\nLa raiz del El area del circulo es de: %f", sqrt(areaCirculo));
+    printf("\nEl area del circulo es de: %f", areaCirculo);
+
+
+
+    sqrAreaCuadrado = sqrt(areaCuadrado);
+    printf("\nLa raiz del area del cuadrado es: %f", sqrAreaCuadrado);
+
+    /*Triangulo*/
+    sqrAreaTriangulo = sqrt(areaTriangulo);
+    printf("\nLa raiz del del area del triangulo es de: %f", sqrAreaTriangulo);
+
+    /*Circulo*/
+    sqrAreaCirculo = sqrt(areaCirculo);
+    printf("\nLa raiz del El area del circulo es de: %f", sqrAreaCirculo);
 }
